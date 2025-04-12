c086c28df6d7ceea97e2d835d898e69907d53773
Daniel <danielbuehrig7501@outlook.com>
2025-04-12 11:42:23 -0400

    Added toString method and confirmed class collaboration with PolicyHolder class

--- Policy.java
+++ Policy.java
@@ -2,13 +2,15 @@ public class Policy
 {
    private String number;
    private String providerName;
+   private PolicyHolder holder;
    private static int instanceCount;
          
    //constructor
-   public Policy(String num, String provider)
+   public Policy(String num, String provider, PolicyHolder owner)
    {
       this.number = num;
       this.providerName = provider;
+      holder = new PolicyHolder(owner);
       instanceCount++;
    }
    /*
@@ -34,6 +36,45 @@ public class Policy
    public String getPolicyNumber()
    {
       return number;
+   }
+   /*
+   The getProviderName method returns the provider name
+   @return provider name
+   */
+   public String getProviderName()
+   {
+      return providerName;
+   }
+   /*
+   The getHolder method returns a copy of the PolicyHolder class referenced by the holder object
+   @return copy of object referenced by holder
+   */
+   public PolicyHolder getHolder()
+   {
+      return new PolicyHolder(holder)
+   }
+   /*
+   the calculateInsuranceFee method calculates and returns the Policy Insurance Fee
+   @return Insurance Fee based on Age, Smoking Status, and BMI
+   */
+   public double calculatePolicyPrice()
+   {
+      double fee = 600; 
+      if (holder.getAge > 50)
+         fee += 75;
+      if (holder.getSmokingStatus.equalsIgnoreCase("smoker"))
+         fee += 100;
+      if (holder.getBMI > 35)
+         fee += (BMI-35)*20;
+      return fee;
+   }
+   /*
+   toString method
+   @return a string indicating the insurance fee
+   */
+   public String toString()
+   {
+      String str = "Policy Price: $%.2f\n", fee;
    }
    /*
    the getIntsanceCount method returns the instance count
